9238f061b2452be855b95db118e3778ee6597fee
gunturrr <gunturwisnu72@gmail.com>
2024-12-12 14:46:04 +0800

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 import daisyui from "daisyui";
 
-module.exports = {
+export default  {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
